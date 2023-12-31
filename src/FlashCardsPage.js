b01a9773bffcda7cbf6339b6f0caac03f4ef2812
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-31 06:54:06 +0400

    m

--- src/FlashCardsPage.js
+++ src/FlashCardsPage.js
@@ -4,7 +4,13 @@ import { fetchCards } from '../services/api';
 const FlashCardsPage = () => {
 const [cards, setCards] = useState([]);
 
+useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchCards();
+      setCards(response.data);
+    };
 
-
-
+    fetchData();
+}, 
+[]);
 export default FlashCardsPage;
