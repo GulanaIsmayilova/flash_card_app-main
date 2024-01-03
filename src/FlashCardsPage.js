1d4b5fafe3d6c30b1aabc89716b2c2d283d5537b
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2024-01-03 14:05:08 +0400

    m

--- src/FlashCardsPage.js
+++ src/FlashCardsPage.js
@@ -4,7 +4,9 @@ import Flashcard from './Flashcard';
 export default function FlashCardsPage({ flashcards }) {
   
   return (
-  
+    <div className="card-grid">
+      {flashcards.map(flashcard => (
+        <Flashcard flashcard={flashcard} key={flashcard.id} />
       ))}
     </div>
   );
