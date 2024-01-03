1decabc6f54203eb7e4dbca0159130d976955177
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2024-01-03 05:51:50 +0400

    m

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,20 +3,17 @@ import React from 'react';
 const projects = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description of Project 1',
-    link: 'https://github.com/yourusername/project1',
+    title: 'FlashCard Application',
+    description: 'Flash Card App for educational purposes,where users can create, view, edit, and delete flash cards. Each card contains information on both sidesand can be used for learning or memorization.',
+    link: 'https://github.com/GulanaIsmayilova/flash_card_app-main.git',
   },
- 
 ];
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome </h1>
-      <p>
-        This is a flashcard application
-      </p>
+      <h1>Welcome</h1>
+      <p>This is a flashcard application</p>
       <ul>
         {projects.map((project) => (
           <li key={project.id}>
