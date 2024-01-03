7d853f267c135a7cc6230ca4636457a6900484bf
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2024-01-03 13:42:16 +0400

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import FlashCardsPage from './FlashCardsPage';
 import HomePage from './HomePage';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
