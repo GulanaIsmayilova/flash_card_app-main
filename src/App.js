635498ff118c147af9dd2659f781a55f35ed890e
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2024-01-03 13:36:22 +0400

    m

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import FlashCardsPage from './FlashCardsPage';
+import HomePage from './HomePage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>FlashCardApp</h1>
-    </div>
-  );
-}
+  const [flashcards, setFlashcards] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://opentdb.com/api.php?amount=10');
+
+        
+                
 export default App;
